247b138093fd8879aa3f49a8a98f2a74b6634da3
Gabo086 <gabrielfwdcostarica@gmail.com>
2026-05-14 16:29:59 -0600

    Guardando

--- frontend/src/routes/Routing.jsx
+++ frontend/src/routes/Routing.jsx
@@ -30,36 +30,36 @@ import Notificaciones from '../pages/Notificaciones'
 const Routing = () => {
   return (
     <Router>
-        <Routes>
-            <Route path='/' element={<LandPage/>}></Route>
-            <Route path='/Register' element = {<Register />} />
-            <Route path='/SolicitudStartup' element = {<SolicitudStartup/>} />
-            <Route path='/Login' element={<Login />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/Mapa' element={<Mapa/>} />
-            <Route path='/PrincipalAceleradoras' element={<PrincipalAceleradoras/>}></Route>
-            <Route path='/PublicoAceleradoras' element={<PublicoAceleradoras/>}></Route>
-            <Route path='/PublicoInversores' element={<PublicoInversores/>}></Route>
-            <Route path='/PublicoStartups' element={<PublicoStartups/>}></Route>
-            <Route path='/PerfilPrivadoStartup' element={<PaginaPerfilPrivadoStartup/>}></Route>
-            <Route path='/SolicitudesPendientesAceleradoras' element={<PaginaSolicitudPendienteAceleradora/>}></Route>
-            <Route path='/SolicitudAceleradora' element={<SolicitudAceleradora/>}></Route>
-            <Route path='/ContactUs' element={<ContactPage/>}></Route>
-            <Route path='/AboutUsPage' element={<AboutUs/>}></Route>
-            <Route path='/StartupPublicLogic' element={<StartupPublicPage/>}></Route>
-            <Route path='/PerfilPrivadoAceleradora' element={<PaginaPerfilPrivadoAceleradora/>}></Route>
-            <Route path='/PerfilPrivadoInversor' element={<PaginaPerfilPrivadoInversor />}></Route>
-            <Route path='/AceleradorasBuscador' element={<AceleradorasBuscador/>}></Route>
-            <Route path="/MensajesAceleradoras" element={<PaginaMensajesAceleradoras/>}></Route>
-            <Route path="/MensajesInversores" element={<PaginaMensajesInversores/>}></Route>
-            <Route path="/MensajesStartups" element={<PaginaMensajesStartups/>}></Route>
-            {/* Rutas Privadas */}
-            <Route path="/DashboardAdmin" element={<PrivateRoutes><PaginaDashboardAdmin/></PrivateRoutes>}></Route>
-            <Route path='/SolicitudesPendientes' element={<PrivateRoutes><PaginaSolicitudesPendientes/></PrivateRoutes>}></Route>
-            <Route path="/GestionarUsuarios" element={<PrivateRoutes><PaginaGestionarUsuarios/></PrivateRoutes>}></Route>
-            <Route path="/PrincipalAdmin" element={<PrivateRoutes><PrincipalAdmin/></PrivateRoutes>}></Route>
-            <Route path="/Notificaciones" element={<PrivateRoutes><Notificaciones/></PrivateRoutes>}></Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<LandPage />}></Route>
+        <Route path='/Register' element={<Register />} />
+        <Route path='/SolicitudStartup' element={<SolicitudStartup />} />
+        <Route path='/Login' element={<Login />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/Mapa' element={<Mapa />} />
+        <Route path='/PrincipalAceleradoras' element={<PrincipalAceleradoras />}></Route>
+        <Route path='/PublicoAceleradoras' element={<PublicoAceleradoras />}></Route>
+        <Route path='/PublicoInversores' element={<PublicoInversores />}></Route>
+        <Route path='/PublicoStartups' element={<PublicoStartups />}></Route>
+        <Route path='/PerfilPrivadoStartup' element={<PaginaPerfilPrivadoStartup />}></Route>
+        <Route path='/SolicitudesPendientesAceleradoras' element={<PaginaSolicitudPendienteAceleradora />}></Route>
+        <Route path='/SolicitudAceleradora' element={<SolicitudAceleradora />}></Route>
+        <Route path='/ContactUs' element={<ContactPage />}></Route>
+        <Route path='/AboutUsPage' element={<AboutUs />}></Route>
+        <Route path='/StartupPublicLogic' element={<StartupPublicPage />}></Route>
+        <Route path='/PerfilPrivadoAceleradora' element={<PaginaPerfilPrivadoAceleradora />}></Route>
+        <Route path='/PerfilPrivadoInversor' element={<PaginaPerfilPrivadoInversor />}></Route>
+        <Route path='/AceleradorasBuscador' element={<AceleradorasBuscador />}></Route>
+        <Route path="/MensajesAceleradoras" element={<PaginaMensajesAceleradoras />}></Route>
+        <Route path="/MensajesInversores" element={<PaginaMensajesInversores />}></Route>
+        <Route path="/MensajesStartups" element={<PaginaMensajesStartups />}></Route>
+        {/* Rutas Privadas */}
+        <Route path="/DashboardAdmin" element={<PrivateRoutes><PaginaDashboardAdmin /></PrivateRoutes>}></Route>
+        <Route path='/SolicitudesPendientes' element={<PrivateRoutes><PaginaSolicitudesPendientes /></PrivateRoutes>}></Route>
+        <Route path="/GestionarUsuarios" element={<PrivateRoutes><PaginaGestionarUsuarios /></PrivateRoutes>}></Route>
+        <Route path="/PrincipalAdmin" element={<PrivateRoutes><PrincipalAdmin /></PrivateRoutes>}></Route>
+        <Route path="/Notificaciones" element={<PrivateRoutes><Notificaciones /></PrivateRoutes>}></Route>
+      </Routes>
     </Router>
   )
 }
