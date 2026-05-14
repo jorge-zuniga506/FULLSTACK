4b7da02ca1ac54f2b1e906e8432c0af881e7ad75
Gabo086 <gabrielfwdcostarica@gmail.com>
2026-05-14 09:16:42 -0600

    cambiosEnpointGetAuth

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/AuthController');
+const { login, getMe } = require('../controllers/AuthController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // Endpoint POST /auth/login
 router.post('/login', login);
+
+// Endpoint GET /auth/me - Protegido
+router.get('/me', authMiddleware, getMe);
 
 module.exports = router;
